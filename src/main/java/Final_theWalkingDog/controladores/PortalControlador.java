63bd810d292fcdb79aec0a00172f9919e34b272c
FrancoMaurino <francomaurino07@gmail.com>
2021-09-01 21:19:02 -0300

    arreglo index

--- src/main/java/Final_theWalkingDog/controladores/PortalControlador.java
+++ src/main/java/Final_theWalkingDog/controladores/PortalControlador.java
@@ -13,4 +13,44 @@ public class PortalControlador {
     public String index(){
         return "index.html";
     }
+     @GetMapping("/")
+    public String contacto(){
+        return "contacto.html";
+    }
+    @GetMapping("/")
+    public String iniciosesionusuario(){
+        return "iniciosesionusuario.html";
+    }
+     @GetMapping("/")
+    public String inisiosesionwalker(){
+        return "inisiosesionwalker.html";
+    }
+    @GetMapping("/")
+    public String otros(){
+        return "otros.html";
+    }
+     @GetMapping("/")
+    public String perfilusuario(){
+        return "perfilusuario.html";
+    }
+    @GetMapping("/")
+    public String perfilwalker(){
+        return "perfilwalker.html";
+    }
+    @GetMapping("/")
+    public String rutaspaseo(){
+        return "perfilwalker.html";
+    }
+     @GetMapping("/")
+    public String usuario(){
+        return "usuario.html";
+    }
+     @GetMapping("/")
+    public String videosinteres(){
+        return "videosinteres.html";
+    }
+     @GetMapping("/")
+    public String walker(){
+        return "walker.html";
+    }
 }
