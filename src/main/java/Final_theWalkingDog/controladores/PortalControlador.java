5ebb9b305497a27e4e07ca78553e1115fbedb0d4
JuampiKeimColl <juampikeim@gmail.com>
2021-09-06 21:58:38 -0300

    Commit controller status

--- src/main/java/Final_theWalkingDog/controladores/PortalControlador.java
+++ src/main/java/Final_theWalkingDog/controladores/PortalControlador.java
@@ -1,56 +1,82 @@
-
 package Final_theWalkingDog.controladores;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
-    
+
     @GetMapping()
-    public String index(){
+    public String index() {
         return "index.html";
     }
-     @GetMapping("/contacto")
-    public String contacto(){
+
+    @GetMapping("/contacto")
+    public String contacto() {
         return "contacto.html";
     }
+
     @GetMapping("/inicosesionusuario")
-    public String iniciosesionusuario(){
+    public String iniciosesionusuario() {
         return "iniciosesionusuario.html";
     }
-     @GetMapping("/inisiosesionwalker")
-    public String inisiosesionwalker(){
+
+    @GetMapping("/inisiosesionwalker")
+    public String inisiosesionwalker() {
         return "inisiosesionwalker.html";
     }
+
     @GetMapping("/otros")
-    public String otros(){
+    public String otros() {
         return "otros.html";
     }
-     @GetMapping("/perfilusuario")
-    public String perfilusuario(){
+
+    @GetMapping("/perfilusuario")
+    public String perfilusuario() {
         return "perfilusuario.html";
     }
+
     @GetMapping("/perfilwalker")
-    public String perfilwalker(){
+    public String perfilwalker() {
         return "perfilwalker.html";
     }
+
     @GetMapping("/rutaspaseo")
-    public String rutaspaseo(){
+    public String rutaspaseo() {
         return "perfilwalker.html";
     }
-     @GetMapping("/usuario")
-    public String usuario(){
+
+    @GetMapping("/usuario")
+    public String usuario() {
         return "usuario.html";
     }
-     @GetMapping("/videosinteres")
-    public String videosinteres(){
+
+    @GetMapping("/videosinteres")
+    public String videosinteres() {
         return "videosinteres.html";
     }
-     @GetMapping("/walker")
-    public String walker(){
+
+    @GetMapping("/walker")
+    public String walker() {
         return "walker.html";
     }
+
+    @PostMapping("/registroUsuario")
+    public String registroUsuario(@RequestParam String email,
+            @RequestParam String nombre, @RequestParam String apellido,
+            @RequestParam String dni, @RequestParam String contrasenia1,
+            @RequestParam String contrasenia2) {
+        System.out.println("Email: " + email);
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Apellido: " + apellido);
+        System.out.println("Dni: " + dni);
+        System.out.println("Clave1: " + contrasenia1);
+        System.out.println("Clave2: " + contrasenia2);
+        return "perfilusuario.html";
+    }
+
 }
