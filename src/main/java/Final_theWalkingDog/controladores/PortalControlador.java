3b9c686f897ce7196646134df1e6f19c4fba92df
FrancoMaurino <francomaurino07@gmail.com>
2021-09-06 23:42:26 -0300

    intento arreglar error registro

--- src/main/java/Final_theWalkingDog/controladores/PortalControlador.java
+++ src/main/java/Final_theWalkingDog/controladores/PortalControlador.java
@@ -65,18 +65,19 @@ public class PortalControlador {
         return "walker.html";
     }
 
-    @PostMapping("/registroUsuario")
+    @PostMapping("/registrarse")
     public String registroUsuario(@RequestParam String email,
             @RequestParam String nombre, @RequestParam String apellido,
             @RequestParam String dni, @RequestParam String contrasenia1,
             @RequestParam String contrasenia2) {
+        
         System.out.println("Email: " + email);
         System.out.println("Nombre: " + nombre);
         System.out.println("Apellido: " + apellido);
         System.out.println("Dni: " + dni);
         System.out.println("Clave1: " + contrasenia1);
         System.out.println("Clave2: " + contrasenia2);
-        return "perfilusuario.html";
+        return "index.html";
     }
 
 }
