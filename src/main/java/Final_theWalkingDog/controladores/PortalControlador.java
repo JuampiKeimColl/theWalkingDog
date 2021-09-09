dcc6d09f6881c287b57f840ec989211f25170916
JuampiKeimColl <juampikeim@gmail.com>
2021-09-08 21:09:56 -0300

    pasando el rato, de la ramaMocoDos

--- src/main/java/Final_theWalkingDog/controladores/PortalControlador.java
+++ src/main/java/Final_theWalkingDog/controladores/PortalControlador.java
@@ -1,6 +1,11 @@
 package Final_theWalkingDog.controladores;
 
+import Final_theWalkingDog.servicios.UsuarioServicios;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
+
+    @Autowired
+    private UsuarioServicios usuarioServicio;
 
     @GetMapping()
     public String index() {
@@ -65,18 +73,25 @@ public class PortalControlador {
         return "walker.html";
     }
 
-      @PostMapping("/registrarse")
-    public String registroUsuario(@RequestParam String email,
+  @PostMapping("/registrarse")
+    public String registroUsuario(ModelMap modelo, @RequestParam String email,
             @RequestParam String nombre, @RequestParam String apellido,
             @RequestParam String dni, @RequestParam String contrasenia1,
             @RequestParam String contrasenia2) {
-        
-        System.out.println("Email: " + email);
-        System.out.println("Nombre: " + nombre);
-        System.out.println("Apellido: " + apellido);
-        System.out.println("Dni: " + dni);
-        System.out.println("Clave1: " + contrasenia1);
-        System.out.println("Clave2: " + contrasenia2);
+
+        try {
+            usuarioServicio.crearUsuario(nombre, apellido, dni, email, contrasenia1, null,null,null);
+        } catch (Exception ex) {
+            modelo.put("error",ex.getMessage());
+            return "usuario.html";
+        }
+
+//        System.out.println("Email: " + email);
+//        System.out.println("Nombre: " + nombre);
+//        System.out.println("Apellido: " + apellido);
+//        System.out.println("Dni: " + dni);
+//        System.out.println("Clave1: " + contrasenia1);
+//        System.out.println("Clave2: " + contrasenia2);
         return "index.html";
     }
 
